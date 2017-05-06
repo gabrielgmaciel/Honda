3eda0b0e28b2e77f3fa79dc0ec5f413770dc6179
System Administrator <root@MacBook-Pro-de-Gabriel.local>
2017-05-06 11:44:31 -0300

    modificar accord

--- accord.php
+++ accord.php
@@ -2,17 +2,35 @@
         <?php include "header.php"; ?>
 
         <body class="login-background" background="imagens/fundo.png">
-            <center>
-                <table width="100%" height="108">
-                    <tr>
-                        <td class="fixed" bgcolor="#1C1C1C" >
-                            <center>
-                                <img src="imagens/accord.jpg"  alt="Cinque Terre" width="1000" height="350">
-                            </center>
-                        </td>
-                    </tr>
+            <div>
+                <center>
+                    <table width="100%" height="108">
+                        <tr>
+                            <td class="fixed" bgcolor="#1C1C1C" >
+                                <center>
+                                    <img src="imagens/accord.jpg"  alt="Cinque Terre" width="1000" height="350">
+                                </center>
+                            </td>
+                        </tr>
+                    </table>
+                </center>
+            </div>
+            <br>
+            <div>
+                <table border="1" bgcolor="#f0ffff" width="50" height="30">
+
+                        <div class="modal-01" width="170" height="110">
+                            <div class="modal-02" width="170" height="110">
+                                <div class="modal-03" width="170" height="110">
+                        <img class="img-rounded" width="170" height="110" src="imagens/accordm.jpg">
+                        <p><h4>Preço: R$: 163.000,00</h4></p>
+                        <button type="button" class="btn btn-success">Comprar</button>
+                                </div>
+                            </div>
+                        </div>
+                    <br><br>
                 </table>
-            </center>
+            </div>
         </body>
 
 </html>
