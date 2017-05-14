74b9a803a9e040a9a4cb938b0ad2b9105c56a691
wanderson <wa4d3rso4@gmail.com>
2017-05-14 02:03:19 -0300

    add cadastro com db/ login com verificação no bd/ erro usuario/senha invalido imprantado

--- conecta.php
+++ conecta.php
@@ -1,3 +1,3 @@
 <?php
-    $conexao= mysqli_connect('localhost', 'root', '', 'bdappweb')
+    $conexao= mysqli_connect('localhost', 'root', '', 'bdappweb');
 ?>
--- s-login.php
+++ s-login.php
@@ -1,30 +1,40 @@
-<?php
+<?php include "conecta.php";
 
 	ini_set ('default_charset','UTF-8');
 	
 	$l1="admin@gmail.com";
 	$pas="admin";
-	
-	$logon=$_GET["logon"];
-	$senha=$_GET["senha"];
+    //variáveis que recebem os dados de login.php
+	$logon=$_POST["login"];
+	$senha=$_POST["senha"];
 
-		if($logon == $l1)
+	if($logon == $l1 && $senha == $pas)
 		{
-			if($senha == $pas)
-			{
-                if($logon == "admin@gmail.com")
-                    {
-                        header ("location: admin.php?logon=admin");
-                    }
-                    else
-                    {
-                        header ("location: index.php?logon=admin");
-                    }
-			}
-		}
+		    header ("location: admin.php?logon=admin");
+        }
 		else
 		{
-			header ("location: login.php?login=0");
+		    //iniciando a sessão
+            session_start();
+            //
+            $sql = "SELECT 'email','senha' FROM cliente WHERE email = '{$logon}' AND senha = '{$senha}'";
+            $result = mysqli_query($conexao,$sql);
+            //
+            $erro = mysqli_error($conexao);
+            echo $erro;
+            mysqli_num_rows($result);
+
+            if(mysqli_num_rows($result) >  0 )
+            {
+                $_SESSION['logon'] = $logon;
+                $_SESSION['senha'] = $senha;
+                header("location: index.php?login=0");
+            } else
+            {
+                unset($_SESSION['logon']);
+                unset($_SESSION['senha']);
+                header ("location: login.php?login=0");
+            }
 		}
 
 ?>
